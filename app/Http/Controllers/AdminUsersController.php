39091a588446086116a9e4bfb69e086217ab5a67
caramongr <liakopoulos@hotmail.com>
2016-11-18 16:00:37 +0200

    users able to persidt

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\User;
+use App\Role;
+use App\Http\Requests\UsersRequest;
 
 class AdminUsersController extends Controller
 {
@@ -16,7 +19,9 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
-        return view('admin.users.index');
+
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
 
     }
 
@@ -28,8 +33,9 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
+        $roles = Role::lists('name','id')->all();
 
-        return view('admin.users.create');
+        return view('admin.users.create',compact('roles'));
     }
 
     /**
@@ -38,9 +44,14 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+
+        User::Create($request->all());
+
+  return redirect('/admin/users');
+
     }
 
     /**
