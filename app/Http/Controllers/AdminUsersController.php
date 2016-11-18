cacc386ef3e2e64fa2c3652d4ad9922641cde7cf
caramongr <liakopoulos@hotmail.com>
2016-11-18 17:45:24 +0200

    users now have photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Photo;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -48,9 +49,32 @@ class AdminUsersController extends Controller
     {
         //
 
-        User::Create($request->all());
+       // User::Create($request->all());
 
-  return redirect('/admin/users');
+$input=$request->all();
+
+        if($file = $request->file('photo_id')) {
+
+
+            $name = time() . $file->getClientOriginalName();
+
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
+
+  //return redirect('/admin/users');
 
     }
 
